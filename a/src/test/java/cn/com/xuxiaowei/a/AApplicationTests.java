1050dc6c9e33c01b249609e7056ce878528a7908
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-21 21:21:53 +0800

    新增模块

---
+++ a/src/test/java/cn/com/xuxiaowei/a/AApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ b/src/test/java/cn/com/xuxiaowei/b/BApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
